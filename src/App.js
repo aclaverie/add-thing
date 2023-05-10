5a571035fe16c29552db9daae62596014473596d
Anthony Claverie <44935840+aclaverie@users.noreply.github.com>
2023-05-10 07:41:31 -0400

    mainly the proper use of the spread oprator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import { useState } from 'react';
 
 function App() {
+  const [arrayItems, setArrayItems] = useState(["Item 1", "Item 2"]);
+
+  function AddingItem(){
+    setArrayItems((prevArrayItems)=>{
+      return [...prevArrayItems, `Item ${prevArrayItems.length+1}`]
+    });
+  }
+  function SubtractingItem(){
+    setArrayItems((prevArrayItems)=>{
+      let modifiedArray = Array.from(prevArrayItems)
+      if(modifiedArray.length === 0){
+
+      } else {
+        modifiedArray.length = modifiedArray.length - 1
+      }
+      return modifiedArray;
+    });
+  }
+
+  function ClearItems(){
+    setArrayItems([]);
+  }
+
+  const displayItems = arrayItems.map((item)=>{
+    return ( <p key={item}>{item}</p> );
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Add Items Exercise
       </header>
+      <div className="App-main">
+        <div className="App-btn">
+          <Button  
+            style={{color: 'white', border: '1px solid #ffffff'}}
+            variant="outlined" onClick={AddingItem}>Add Item</Button>
+          <Button  
+            style={{color: 'white', border: '1px solid #ffffff'}}
+            variant="outlined" onClick={SubtractingItem}>Subtract Item</Button>
+          <Button 
+            style={{color: 'white', border: '1px solid #ffffff'}}
+            variant="outlined" onClick={ClearItems}>Clear Items</Button>
+        </div>
+        <div>
+          { displayItems }
+        </div>
+      </div>
+      <div className="App-footer">
+        <div className="footer-info">
+          Powered By React!
+        </div>
+      </div>
     </div>
   );
 }
